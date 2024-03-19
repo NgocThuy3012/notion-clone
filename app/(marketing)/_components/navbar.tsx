9493a7eb55781e917ef6cfae16bd4c6f2b6937d0
NgocThuy3012 <tranngocthuy3012@gmail.com>
2024-03-19 09:29:59 +0700

    done login with github

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,13 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +22,20 @@ export const Navbar = () => {
     >
       <Logo />
       <div className=" md:ml-auto md:justify-end justify-between items-center flex w-full gap-x-2 ">
+        {isLoading && <p> Loading...</p>}
+
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://regular-raven-59.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
